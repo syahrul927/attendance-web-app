bc986f9b8451554da875baf1b8a3fcca4ef91ccd
ajirusmayran <aji.rusmayran@quadras.co.id>
2020-11-12 12:02:57 +0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          WEB ABSENSI STMIK BANI SALEH <br/>
+          BY AJI RUSMAYRAN
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
